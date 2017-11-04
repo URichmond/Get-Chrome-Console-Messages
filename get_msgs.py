7888a8b30ab168bc71b864020010ab738932bc93
George Flanagin <me@georgeflanagin.com>
2017-11-04 15:28:53 -0400

    Pythonic revisions, part one.

--- get_msgs.py
+++ get_msgs.py
@@ -1,18 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+A program to read the console messages from Chrome, using the Selenium
+test driver.
+"""
+
 import argparse
+import collections
+from   collections import OrderedDict
+from   collections import namedtuple
 import json
 import os
 import re
 import sys
 import time
-from collections import OrderedDict
-from collections import namedtuple
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+# Credits
+__author__ =        'Eric Palmer'
+__copyright__ =     'Copyright 2017, University of Richmond'
+__version__ =       '2.1'
+__maintainer__ =    'Eric Palmer'
+__email__ =         'epalmer@richmond.edu'
+__status__ =        'production'
+__licence__ =       'http://www.gnu.org/licenses/gpl-3.0.en.html'
 
-# TODO add doc strings
+
 
 def extract_mc_url(re_pattern, msg):
     '''return extracted mixed content url as string if found else empty string'''
@@ -28,25 +44,15 @@ def extract_mc_url(re_pattern, msg):
 
 
 def results2tsv(results):
-    tsv_list = list()
-    tsv_list.append("\t".join(['url', 'level', 'message', 'source', 'mc_url', 'timestamp']))
-    for url in results:
+    tsv_list = [("\t".join(['url', 'level', 'message', 'source', 'mc_url', 'timestamp']))]
 
+    for url in results:
         if len(results[url]) == 0:
-            tsv_row = list()
-            tsv_row.append(url)
-            tsv_row.append('INFO')
-            tsv_row.append('No log msg found for this url')
-            tsv_list.append("\t".join(tsv_row))
+            tsv_list.append("\t".join([url, 'INFO', 'No log msg found for this url']))
         else:
             for logmsg in results[url]:
-                tsv_row = list()
-                tsv_row.append(url)
-                tsv_row.append(logmsg['level'])
-                tsv_row.append(logmsg['message'])
-                tsv_row.append(logmsg['source'])
-                tsv_row.append(logmsg['mc_url'])
-                tsv_row.append(logmsg['timestamp'])
+                tsv_row = "\t".join([url, logmsg['level'], logmsg['message']
+                    logmsg['source'], logmsg['mc_url'], logmsg['timestamp']])
                 tsv_list.append("\t".join(tsv_row))
 
     return tsv_list
@@ -67,28 +73,18 @@ def cli_parse():
 
     parser.add_argument("-j", "--json", help="Json formatted results file", action="store_true")
     parser.add_argument("-t", "--tsv",
-                        help="Tab separated values (tsv) formatted results file (default output_format",
+                        help="Tab separated values (tsv) formatted results file (default output_format)",
                         action="store_true")
 
     parser.add_argument("-v", "--verbose",
                         help="Outputs to stdout urls being processed and other useful information",
                         action="store_true")
-    parser.parse_args()
     args = parser.parse_args()
 
     if args.json and args.tsv:
         raise Exception("Program argument conflict - pick json or tsv file formats not both.")
 
-    output_format = 'tsv'
-    if not args.json and not args.tsv:
-        # default output_format
-        output_format = 'tsv'
-
-    if args.json:
-        output_format = 'json'
-
-    if args.tsv:
-        output_format = 'tsv'
+    output_format = 'json' if args.json else 'tsv'
 
     cli_args = namedtuple('cli_args', 'chromebrowser chromedriver infile outfile output_format verbose')
     return cli_args(chromebrowser=args.chromebrowser,
@@ -99,7 +95,7 @@ def cli_parse():
                     verbose=args.verbose)
 
 
-def setup_chrome_options(cli_args):
+def setup_chrome_options(cli_args:object) -> object:
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.binary_location = cli_args.chromebrowser
@@ -112,16 +108,20 @@ def setup_chrome_options(cli_args):
     return driver
 
 
-def get_console_msgs(driver, urls, re_pattern, verbose):
-    results = OrderedDict()
+def get_console_msgs(driver:object, urls:str, re_pattern, verbose:bool) -> collections.OrderedDict:
+    results = collections.OrderedDict()
     if verbose:
         print("Getting console log records from urls")
     for ct, url in enumerate(urls):
-        if len(url.strip()) == 0 or url.strip()[:1] == "#":
-            # ignore blank lines in file or comments in file
+        if not url or url[1] == "#":
             continue
-        # TODO try except for invalid urls
-        driver.get(url)
+
+        try:
+            driver.get(url)
+        except Exception as e:
+            print(str(e))
+            continue
+
         log = driver.get_log('browser')
         for item in log:
             timestamp = item['timestamp']
@@ -138,10 +138,10 @@ def program_exit(driver, verbose):
     driver.close()
     if verbose:
         print("Program exit")
-    sys.exit(0)
+    sys.exit(os.EX_OK)
 
 
-def json_output(output_file, results, verbose):
+def json_output(output_file:str, results:object, verbose:bool):
     if verbose:
         print("Preparing and saving json output")
     json_str = results2json(results)
@@ -162,10 +162,14 @@ def tsv_output(output_file, results, verbose):
 
 def main():
     re_pattern = "(http:.*)(\'. This content should also be served over HTTPS\.|. This request has been blocked; the content must be served over HTTPS\.$)"
+    re.compile(re_pattern)
 
-    cli_args = cli_parse()
-
-    driver = setup_chrome_options(cli_args)
+    try:
+        cli_args = cli_parse()
+        driver = setup_chrome_options(cli_args)
+    except Exception as e:
+        print(str(e))
+        sys.exit(os.EX_DATA)
 
     input_file = cli_args.infile
     output_file = cli_args.outfile
@@ -180,12 +184,11 @@ def main():
         print(cli_args.infile)
         print(cli_args.outfile)
 
-    urls = list()
+    urls = []
     if verbose:
         print("Opening input file")
     with open(input_file, 'r') as infile:
-        for line in infile:
-            urls.append(line.strip())
+        urls = [ _.strip() for _ in infile.readlines() ]
 
     results = get_console_msgs(driver, urls, re_pattern, verbose)
 
