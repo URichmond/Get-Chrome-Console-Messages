ca20edc223ebeb07d833c15e75c5f950f9e0a1b8
Eric Palmer <epalmer@richmond.edu>
2017-11-07 11:34:07 -0500

    Many changes to make file more pythonic.  Hey I'm learning...

--- get_msgs.py
+++ get_msgs.py
@@ -7,8 +7,7 @@ test driver.
 
 import argparse
 import collections
-from   collections import OrderedDict
-from   collections import namedtuple
+from collections import namedtuple
 import json
 import os
 import re
@@ -20,17 +19,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 # Credits
-__author__ =        'Eric Palmer'
-__copyright__ =     'Copyright 2017, University of Richmond'
-__version__ =       '2.1'
-__maintainer__ =    'Eric Palmer'
-__email__ =         'epalmer@richmond.edu'
-__status__ =        'production'
-__licence__ =       'http://www.gnu.org/licenses/gpl-3.0.en.html'
+__author__ = 'Eric Palmer'
+__copyright__ = 'Copyright 2017, University of Richmond'
+__version__ = '1.1'
+__maintainer__ = 'Eric Palmer'
+__email__ = 'epalmer@richmond.edu'
+__status__ = 'production'
+__licence__ = 'https://opensource.org/licenses/MIT'
 
 
-
-def extract_mc_url(re_pattern:str, msg:str) -> str:
+def extract_mc_url(re_pattern: str, msg: str) -> str:
     """
     return extracted mixed content url as string if found else empty string
     """
@@ -38,16 +36,16 @@ def extract_mc_url(re_pattern:str, msg:str) -> str:
     if "Mixed Content:" in msg:
         results = re.search(re_pattern, msg)
         if results and (len(results.group()) > 1):
-            return(results.group(1))
+            return results.group(1)
         else:
             return "Error parsing message for mc url"
     else:
         return ""
 
 
-def results2tsv(results:object) -> list:
+def results2tsv(results: object) -> list:
     """
-    Convert the results object to a list of strings.
+    Convert the results object to a tsv list of strings.
     """
 
     tsv_list = ["\t".join(['url', 'level', 'message', 'source', 'mc_url', 'timestamp'])]
@@ -58,8 +56,8 @@ def results2tsv(results:object) -> list:
         else:
             for logmsg in results[url]:
                 tsv_row = "\t".join([url, logmsg['level'], logmsg['message'],
-                    logmsg['source'], logmsg['mc_url'], logmsg['timestamp']])
-                tsv_list.append("\t".join(tsv_row))
+                                     logmsg['source'], logmsg['mc_url'], logmsg['timestamp']])
+                tsv_list.append(tsv_row)
 
     return tsv_list
 
@@ -90,11 +88,12 @@ def cli_parse() -> tuple:
     args = parser.parse_args()
 
     if args.json and args.tsv:
-        raise Exception("Program argument conflict - pick json or tsv file formats not both.")
+        raise Exception("Program argument conflict - pick json or tsv file format, not both.")
 
     output_format = 'json' if args.json else 'tsv'
 
-    cli_args = namedtuple('cli_args', 'chromebrowser chromedriver infile outfile output_format verbose')
+    cli_args = namedtuple('cli_args',
+                          'chromebrowser chromedriver infile outfile output_format verbose')
     return cli_args(chromebrowser=args.chromebrowser,
                     chromedriver=args.chromedriver,
                     infile=args.infile,
@@ -103,7 +102,7 @@ def cli_parse() -> tuple:
                     verbose=args.verbose)
 
 
-def setup_chrome_options(cli_args:object) -> object:
+def setup_chrome_options(cli_args: object) -> object:
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.binary_location = cli_args.chromebrowser
@@ -116,7 +115,8 @@ def setup_chrome_options(cli_args:object) -> object:
     return driver
 
 
-def get_console_msgs(driver:object, urls:str, re_pattern, verbose:bool) -> collections.OrderedDict:
+def get_console_msgs(driver: object, urls: str, re_pattern,
+                     verbose: bool) -> collections.OrderedDict:
     results = collections.OrderedDict()
     verbose and print("Getting console log records from urls")
     for ct, url in enumerate(urls):
@@ -126,7 +126,9 @@ def get_console_msgs(driver:object, urls:str, re_pattern, verbose:bool) -> colle
         try:
             driver.get(url)
         except Exception as e:
+            print("Unable to get console messages for url: " + url)
             print(str(e))
+            print("continuing...")
             continue
 
         log = driver.get_log('browser')
@@ -140,7 +142,7 @@ def get_console_msgs(driver:object, urls:str, re_pattern, verbose:bool) -> colle
     return results
 
 
-def json_output(output_file:str, results:object, verbose:bool) -> None:
+def json_output(output_file: str, results: object, verbose: bool) -> None:
     """
     Saves the console messages as JSON, perhaps verbosely.
 
@@ -155,7 +157,7 @@ def json_output(output_file:str, results:object, verbose:bool) -> None:
         outfile.write(json.dumps(results) + "\n")
 
 
-def tsv_output(output_file:str, results:object, verbose:bool) -> None:
+def tsv_output(output_file: str, results: object, verbose: bool) -> None:
     """
     Saves the console messages as tab separated values.
 
@@ -172,15 +174,15 @@ def tsv_output(output_file:str, results:object, verbose:bool) -> None:
 
 
 def main():
-    re_pattern = "(http:.*)(\'. This content should also be served over HTTPS\.|. This request has been blocked; the content must be served over HTTPS\.$)"
-    x = re.compile(re_pattern)
+    re_pattern = re.compile(
+        "(http:.*)(\'. This content should also be served over HTTPS\.|. This request has been blocked; the content must be served over HTTPS\.$)")
 
     try:
         cli_args = cli_parse()
         driver = setup_chrome_options(cli_args)
     except Exception as e:
         print(str(e))
-        sys.exit(os.EX_DATA)
+        sys.exit(os.EX_DATAERR)
 
     input_file = cli_args.infile
     output_file = cli_args.outfile
@@ -188,17 +190,17 @@ def main():
     verbose = cli_args.verbose
 
     if verbose:
-        print(cli_args.verbose)
-        print(cli_args.output_format)
-        print(cli_args.chromebrowser)
-        print(cli_args.chromedriver)
-        print(cli_args.infile)
-        print(cli_args.outfile)
+        print("verbose mode")
+        print("output format:" + output_format)
+        print("chromebrowser:" + cli_args.chromebrowser)
+        print("chromedriver:" + cli_args.chromedriver)
+        print("input file: " + input_file)
+        print("output file: " + output_file)
 
-    urls = []
     verbose and print("Opening input file")
+    # test comment lines
     with open(input_file) as infile:
-        urls = [ _.strip() for _ in infile.readlines() ]
+        urls = [_.strip() for _ in infile.readlines()]
 
     results = get_console_msgs(driver, urls, re_pattern, verbose)
 
